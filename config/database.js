d03d8c5da839fe50e00e789a6f59d5569cf23b19
aleksov <aleksov101@gmail.com>
2020-10-18 20:35:05 +0300

    fix async problem with admin seed

--- config/database.js
+++ config/database.js
@@ -3,22 +3,22 @@ const User = require('../models/User');
 
 mongoose.Promise = global.Promise;
 async function initializeDataBase(connectionString) {
-    await mongoose.connect(connectionString, {
+    mongoose.connect(connectionString, {
         useUnifiedTopology: true,
         useCreateIndex: true,
         useNewUrlParser: true,
     });
 
-    const db = await mongoose.connection;
-
+    const db = mongoose.connection;
+    
     db.once('open', err => {
         if (err) {
             console.log(err);
         }
-
+        
         User.seedAdmin()
             .then(() => {
-                console.log('Data base ready');
+                console.log('Admin seeded!');
             }).catch((err) => {
             console.error(err);
         })
